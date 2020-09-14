84c71671ea76b3fa1dfdac8647abb6d1678c2416
gogogo97 <gogogo97_0_@hotmail.co.th>
2020-09-14 16:45:33 +0700

    Add ATMConfig

---
+++ src/main/java/atm/ATMConfig.java
@@ -0,0 +1,22 @@
+@Configuration
+public class ATMConfig {
+    @Bean
+    public DataSource dataSource() {
+        return new DataSourceFile("customers.txt");
+    }
+
+    @Bean
+    public Bank bank() {
+        return new Bank("My Bank", dataSource());
+    }
+
+    @Bean
+    public ATM atm() {
+        return new ATM(bank());
+    }
+
+    @Bean
+    public AtmUI atmUI() {
+        return new AtmUI(atm());
+    }
+}
